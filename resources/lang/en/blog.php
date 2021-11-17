0154921d1b059eab86c91a36645ea3271fc7db16
Ngoc Bao <63847215+ngocbao1512@users.noreply.github.com>
2021-11-17 21:01:46 +0700

    update code 6h01 17/11 (#2)

--- resources/lang/en/blog.php
+++ resources/lang/en/blog.php
@@ -2,6 +2,8 @@
 return [
     'all_blog' => 'All Blog',
     'add_blog' => 'Add Blog',
+    'schedule' => 'Schedule ',
+    'public' => 'Public',
     'title' => 'Title',
     'content' => 'Content',
     'author' => 'Author',
@@ -12,5 +14,42 @@ return [
     'wait_verify' => 'Wait Verify',
     'be_rejected' => 'Be Rejected',
     'no_data' => 'No Data',
+    'create-' => [
+        'blog' => 'Create Blog',
+        'success' => 'Create Blog Success',
+        'fail' => 'Create Blog Fail',
+        'wrong' => 'Some Thing Went Wrong When You Try Create Blog',
+    ],
+    'edit-' => [
+        'blog' => 'Edit Blog',
+        'success' => 'Edit Blog Success',
+        'fail' => 'Edit Blog Fail',
+        'wrong' => 'Some Thing Went Wrong When You Try Edit Blog',
+    ],
+    'delete-' => [
+        'blog' => 'Delete Blog',
+        'success' => 'Delete Blog Success',
+        'fail' => 'Delete Blog Fail',
+        'wrong' => 'Some Thing Went Wrong When You Try Delete Blog',
+    ],
+    'title-' => [
+        'not_null' => 'Title Is Not Null',
+        'at_least_3_charater' => 'Title At Least Three Character',
+        'no_more_than_144_character' => 'Title should not exceed 144 characters',
+        'wrong' => 'Some Thing Wrong In Your Title. please check again!!!',
+    ],
+    'content-' => [
+        'not_null' => 'Content Is Not Null',
+        'wrong' => 'Some Thing Wrong In Your Content. please check again!!!',
+    ],
+    'image-' => [
+        'is_image' => 'File input must be Image not Document or something like that.',
+        'wrong' => 'Some Thing Went Wrong In Your Field Image',
+    ],
+    'publish_date-' => [
+        'not_null' => 'Please schedule a publish date',
+        'wrong' => 'Some Thing Went Wrong In Your field publish date. please check again!!!', 
+    ],
+    
     
 ];
--- resources/views/admin/blog/script.blade.php
+++ resources/views/admin/blog/script.blade.php
@@ -1,8 +1,3 @@
 <script>
-$('#datepicker').datepicker();
-$('#datepicker').on('changeDate', function() {
-    $('#my_hidden_input').val(
-        $('#datepicker').datepicker('getFormattedDate')
-    );
-});
+   
 </script>
