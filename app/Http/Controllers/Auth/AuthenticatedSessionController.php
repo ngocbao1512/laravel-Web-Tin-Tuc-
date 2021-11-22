6d54b304a124ae1bd2ccc04c54f309f3ab8293b7
ngocbao1512 <tranngocbao089@gmail.com>
2021-11-22 07:55:23 +0700

    update code 22/11

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,6 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request)
     {
-        dd($request);
         $request->authenticate();
 
         $request->session()->regenerate();
