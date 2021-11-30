d9806f88a397cdaa304fec43521e904a625aa6f9
ngoc bao <tranngocbao089@gmail.com>
2021-11-30 10:16:59 +0700

    update code 30/11 10h16

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://localhost:8089/userfiles'),
 
     'asset_url' => env('ASSET_URL', null),
 
